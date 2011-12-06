a71c7ad7d846e41a681a2f92b77757b3a469e0f1
unknown <andrea@URAHARA.(none)>
2011-12-06 13:47:35 +0100

    removed headers.rb

--- lib/headers.rb
+++
@@ -1,9 +0,0 @@
-
-require 'require_all'
-
-require_all File.join(File.dirname(__FILE__), 'core')
-#require_all File.join(File.dirname(__FILE__), 'sdl')
-require_all File.join(File.dirname(__FILE__), 'gosu')
-
-
-
